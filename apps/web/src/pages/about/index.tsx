d77f32789560b7b40850c51a2dfb431b683453ea
Takaki Takeuchi <takaki@takeu.ch>
2022-06-14 23:55:04 +0900

    feat: setup header with chakra-ui

---
+++ apps/web/src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from '@chakra-ui/react';
+import { NextPage } from 'next';
+
+const AboutPage: NextPage = () => {
+  return (
+    <Box>
+      <Heading>About</Heading>
+    </Box>
+  );
+};
+
+export default AboutPage;
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,7 +1,25 @@
 module.exports = {
   extends: ['next', 'prettier'],
+  plugins: ['simple-import-sort', 'unused-imports'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
+
+    // for simple-import-sort
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+
+    // for unused-imports
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 };
